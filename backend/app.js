b7eff12a0a962af9d318f2864e56ecd24b33fc5a
Pratap Sharma <sharma.pratap22@gmail.com>
2023-11-23 19:29:23 +0545

    create-api-send-mail

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,22 @@
 const express = require("express");
-
+const errorMiddleware = require("./middleware/error");
 const app = express();
 
 const appRouter = require("./controller");
+const LWPError = require("./utils/error");
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Home");
 });
 
+app.get("/test", (req, res) => {
+  throw new LWPError();
+});
+
 app.use("/api/v1", appRouter);
 
-app.get("/test", (req, res) => {
-  res.send("Working");
-});
+app.use(errorMiddleware);
 
 module.exports = app;
--- backend/controller/user-controller.js
+++ backend/controller/user-controller.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const { sendMail } = require("../utils/mailer");
 
 const userRouter = express.Router();
 
@@ -6,8 +8,30 @@ userRouter.get("/", (req, res) => {
   res.send("userRouter");
 });
 
-userRouter.get("/create", (req, res) => {
-  res.send("Create");
+userRouter.post("/create", (req, res) => {
+  const { name, email, password } = req.body;
+  //TODO - Name, email, password validation
+
+  const activationToken = createActivationToken({ name, email, password });
+  // TODO change the port
+  const activationUrl = `http://localhost:8080/api/v1/user/activation/?token=${activationToken}`;
+  sendMail({
+    email: email,
+    subject: "Please Activate Your Account",
+    message: `To activate your account click the link ${activationUrl}`,
+  });
+  res.status(200).json({ success: true, message: "User Activation link sent" });
 });
+
+userRouter.get("/activation", (req, res) => {
+  const { token } = req.query;
+  console.log("Token :: ", token);
+
+  res.status(200).send("User Activated");
+});
+
+const createActivationToken = (userData) => {
+  return jwt.sign(userData, process.env.JWT_SECRET, { expiresIn: "5m" });
+};
 
 module.exports = userRouter;
